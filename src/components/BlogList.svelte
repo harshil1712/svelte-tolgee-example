dac57d2edfefdaf14fbae0795e33d7db55d928a1
Harshil <ghagrawal17@gmail.com>
2024-05-15 09:22:18 +0200

    ⚡Update Readme and other house keeping

--- src/components/BlogList.svelte
+++ src/components/BlogList.svelte
@@ -1,12 +1,12 @@
 <script>
 	export let posts;
-  </script>
+</script>
 
-  <div>
+<div>
 	{#each posts.summaries as post}
-	  <a href={`/post/${post.id}`} class="block bg-white p-6 rounded-lg shadow-lg mb-6">
-		<h2 class="text-xl font-bold mb-2">{post.title}</h2>
-		<p>{post.summary}</p>
-	  </a>
+		<a href={`/post/${post.id}`} class="block bg-white p-6 rounded-lg shadow-lg mb-6">
+			<h2 class="text-xl font-bold mb-2">{post.title}</h2>
+			<p>{post.summary}</p>
+		</a>
 	{/each}
-  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Navbar from '../components/Navbar.svelte';
-	import BlogList from '../components/BlogList.svelte';
 </script>
 
 <style global>
@@ -10,6 +9,6 @@
 <div class="min-h-screen bg-gray-100">
 	<Navbar />
 	<div class="container mx-auto px-4 py-8">
-	  <slot/>
+		<slot/>
 	</div>
-  </div>
+</div>
